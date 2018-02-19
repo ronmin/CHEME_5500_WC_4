d9195132a125653d5c984748a34eb11fb2cb72c2
ronmin <35746685+ronmin@users.noreply.github.com>
2018-02-18 20:48:02 -0500

    Create mundaneMath.py

---
+++ mundaneMath.py
@@ -0,0 +1,9 @@
+#! python
+# mundaneMath.py - Calculates the sum of all even numbers between 1 and 100.
+
+def main():
+    summ = sum(range(2, 101, 2))
+    return summ
+
+if __name__ = '__main__':
+    print('The sum of all even numbers between 1 and 100 are %s.' % (summ))
