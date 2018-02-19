51236ba83fc3db9fb8c3415ca9e3116404a0d437
ronmin <35746685+ronmin@users.noreply.github.com>
2018-02-18 20:52:12 -0500

    Update mundaneMath.py

--- mundaneMath.py
+++ mundaneMath.py
@@ -5,5 +5,5 @@ def main():
     summ = sum(range(2, 101, 2))
     return summ
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     print('The sum of all even numbers between 1 and 100 is %s.' % (summ))
